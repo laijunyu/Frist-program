5ed12475e25dd0c546e19e74b2206a9cadc57978
laijunyu <61925505+laijunyu@users.noreply.github.com>
2022-07-04 11:19:57 +0800

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <Windows.h>
+int ad(int u,int o)
+{
+    u = o++;
+    return (u);
+
+}
+int main(void)
+{
+    
+    int a,t;
+    a = 9;
+    printf("%d\n", a);
+    int d;
+    d = 6;
+    std::cout << "Hello World!\n";
+    printf ("qingshurushuzi\n");
+    printf ("%d\n", d);
+    Sleep (500);
+    printf("%d", ad);
+    d = getchar();
+    if (a < d)
+    {
+        printf("hahaha");
+    }
+    else
+    {
+        printf("wowowow");
+    }
+    Sleep(5000);
+    return 0;
+}
